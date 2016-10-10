9e83d03c20e8e9e1fce6605dd2a69460962e2e01
Samuele Bacchetta <samuele1988@gmail.com>
2016-10-10 22:45:59 +0200

    login

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php
+<html>
+<head>
+	<title>PHP test page</title>
+	<link rel="stylesheet" type="text/css" href="styles/index.css">
+</head>
+
+<body>
+<?php 
+	print '<a href="login/login.php" target=_blank">This is a link</a>';
+?>
+<a href="login/login.php" target="_blank">This is a link</a>
+	
+</body>
+</html>	
---
+++ login/login.php
@@ -0,0 +1,110 @@
+<?php
+ob_start();
+session_start();
+?>
+
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="styles/login.css">
+</head>
+
+<body>
+
+
+<form id='login' action='login.php' method='post' accept-charset='UTF-8'>
+<fieldset >
+<legend>Login</legend>
+<input type='hidden' name='submitted' id='submitted' value='1'/>
+ 
+<label for='username' >UserName:</label>
+<input type='text' name='username' id='username'  maxlength="50" />
+<br>
+<label for='password' >Password:</label>
+<input type='password' name='password' id='password' maxlength="50" />
+ 
+<input type='submit' name='Submit' value='Submit' />
+
+<?php 
+/*
+if(isset($_POST['submitted']))
+{
+	if($fgmembersite->Login())
+	{
+		$fgmembersite->RedirectToURL("login-home.php");
+	}
+	
+}
+*/
+if(isset($_POST['submitted'])){
+	Login();
+}
+
+function Login()
+{
+	if(empty($_POST['username']))
+	{
+		//$this->HandleError("UserName is empty!");
+		return false;
+	}
+	 
+	if(empty($_POST['password']))
+	{
+		//$this->HandleError("Password is empty!");
+		return false;
+	}
+	 
+	$username = trim($_POST['username']);
+	$password = trim($_POST['password']);
+	 
+	if(!CheckLoginInDB($username,$password))
+	{
+		return false;
+	}
+	if(isset($_SESSION['user'])){
+		session_unset();
+		session_destroy();
+	}
+	session_start();
+	$_SESSION['user'] = $username;
+	print_r($_SESSION);
+	 
+	//$_SESSION[$this->GetLoginSessionVar()] = $username;
+	 
+	return true;
+}
+
+function CheckLoginInDB($username,$password)
+{
+	/*
+	if(!$this->DBLogin())
+	{
+		$this->HandleError("Database login failed!");
+		return false;
+	}
+	$username = $this->SanitizeForSQL($username);
+	$pwdmd5 = md5($password);
+	$qry = "Select name, email from $this->tablename ".
+			" where username='$username' and password='$pwdmd5' ".
+			" and confirmcode='y'";
+	 
+	$result = mysql_query($qry,$this->connection);
+	 
+	if(!$result || mysql_num_rows($result) <= 0)
+	{
+		$this->HandleError("Error logging in. ".
+				"The username or password does not match");
+		return false;
+	}
+	return true;
+	*/
+	return true;
+}
+
+?>
+ 
+</fieldset>
+</form>
+	
+</body>
+</html>	
